b434c171a2089b3fce730e7d545e09e689c011f3
Shivender Singh <104500591+Shivendra437@users.noreply.github.com>
2023-01-27 10:24:15 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-a
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
